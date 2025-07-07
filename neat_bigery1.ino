c915ebaa580a4e63fdd7f7b078a90b6b2b71153b
Sa12345678434 <444806839@kku.edu.sa>
2025-07-08 01:05:24 +0300

    Add files via upload

---
+++ neat_bigery1.ino
@@ -0,0 +1,37 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(7,INPUT);
+  pinMode(4,INPUT);
+  pinMode(2,INPUT);
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(8,OUTPUT);
+}
+
+void loop()
+{
+  if(digitalRead(7)==HIGH){
+    digitalWrite(13,HIGH);
+    delay(2000);
+    digitalWrite(13,LOW);
+    delay(2000);
+  }
+  else if(digitalRead(4)==HIGH){
+    digitalWrite(12,HIGH);
+    delay(2000);
+    digitalWrite(12,LOW);
+    delay(2000);
+  }
+  else if(digitalRead(2)==HIGH){
+    digitalWrite(8,HIGH);
+    delay(2000);
+    digitalWrite(8,LOW);
+    delay(2000);
+  }
+  else{
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(13,LOW);
+  }}
